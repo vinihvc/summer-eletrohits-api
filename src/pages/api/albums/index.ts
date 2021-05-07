c44f0b8e84b7926b3b529dc4f82c0056548d5807
Vinicius Vicentini <viniciusvicentini@live.com>
2021-05-07 17:57:30 -0300

    return albums and songs in '/album' api

--- src/pages/api/albums/index.ts
+++ src/pages/api/albums/index.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import cors from 'helpers/init-middleware'
 
 import albums from 'db/albums'
+import songs from 'db/songs'
 
 export default async (
   req: NextApiRequest,
@@ -10,5 +11,12 @@ export default async (
 ) => {
   await cors(req, res)
 
-  res.status(200).json(albums)
+  const albumsAndSongs = albums.map((album) => {
+    return {
+      ...album,
+      songs: songs.filter((song) => album.id === song.albumId)
+    }
+  })
+
+  res.status(200).json(albumsAndSongs)
 }

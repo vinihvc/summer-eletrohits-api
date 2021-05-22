235835a154911813d06b6fa9e49ca26829867c48
Vinicius Vicentini <viniciusvicentini@live.com>
2021-05-22 19:14:44 -0300

    updating thumb image url

--- src/db/albums.ts
+++ src/db/albums.ts
@@ -3,112 +3,112 @@ export default [
     id: 1,
     name: 'Summer Eletrohits',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/3/39/Summer_Eletrohits.jpg/400px-Summer_Eletrohits.jpg',
-    releaseDate: '2005-01-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562468/summer-eletrohits/01_hkbpyn.jpg',
+    releaseDate: '2005-01-01T02:00:00.000Z'
   },
   {
     id: 2,
     name: 'Summer Eletrohits 2',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/a/ad/Summer_Eletrohits_2.jpg/200px-Summer_Eletrohits_2.jpg',
-    releaseDate: '2005-12-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562468/summer-eletrohits/02_fceoas.jpg',
+    releaseDate: '2005-12-01T02:00:00.000Z'
   },
   {
     id: 3,
     name: 'Summer Eletrohits 3',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/a/a6/Summer_Eletrohits_3.jpg/200px-Summer_Eletrohits_3.jpg',
-    releaseDate: '2006-12-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562468/summer-eletrohits/03_pihtbq.jpg',
+    releaseDate: '2006-12-01T02:00:00.000Z'
   },
   {
     id: 4,
     name: 'Summer Eletrohits 4',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/4/45/Summer_Eletrohits_4.jpg/200px-Summer_Eletrohits_4.jpg',
-    releaseDate: '2007-12-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562468/summer-eletrohits/04_pisr8x.jpg',
+    releaseDate: '2007-12-01T02:00:00.000Z'
   },
   {
     id: 5,
     name: 'Summer Eletrohits 5',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/f/ff/Summer_Eletrohits_5.jpg/200px-Summer_Eletrohits_5.jpg',
-    releaseDate: '2008-12-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562468/summer-eletrohits/05_t7denu.jpg',
+    releaseDate: '2008-12-01T02:00:00.000Z'
   },
   {
     id: 6,
     name: 'Summer Eletrohits 6',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/9/94/Summer_Eletrohits_6.jpg/200px-Summer_Eletrohits_6.jpg',
-    releaseDate: '2009-12-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562468/summer-eletrohits/06_ylw1p3.jpg',
+    releaseDate: '2009-12-01T02:00:00.000Z'
   },
   {
     id: 7,
     name: 'Summer Eletrohits 7',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/9/9b/Summer_Eletrohits_7.jpg/200px-Summer_Eletrohits_7.jpg',
-    releaseDate: '2010-12-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562468/summer-eletrohits/07_ylajb2.jpg',
+    releaseDate: '2010-12-01T02:00:00.000Z'
   },
   {
     id: 8,
     name: 'Summer Eletrohits 8',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/3/31/Summer_Eletrohits_8.jpg/200px-Summer_Eletrohits_8.jpg',
-    releaseDate: '2011-12-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562468/summer-eletrohits/08_jvahw5.jpg',
+    releaseDate: '2011-12-01T02:00:00.000Z'
   },
   {
     id: 9,
     name: 'Summer Eletrohits 9',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/f/f8/Summer_Eletrohits_9.jpg/200px-Summer_Eletrohits_9.jpg',
-    releaseDate: '2013-02-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562469/summer-eletrohits/09_ambjda.jpg',
+    releaseDate: '2013-02-01T02:00:00.000Z'
   },
   {
     id: 10,
     name: 'Summer Eletrohits 10',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/2/27/Summer_Eletrohits_10.jpg/200px-Summer_Eletrohits_10.jpg',
-    releaseDate: '2014-03-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562469/summer-eletrohits/10_l9xhrd.jpg',
+    releaseDate: '2014-03-01T02:00:00.000Z'
   },
   {
     id: 11,
     name: 'Summer Eletrohits 11 (2015)',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/5/54/Summer_Eletrohits_2015.png/200px-Summer_Eletrohits_2015.png',
-    releaseDate: '2015-01-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562469/summer-eletrohits/11_yary4z.jpg',
+    releaseDate: '2015-01-01T02:00:00.000Z'
   },
   {
     id: 12,
     name: 'Summer Eletrohits 12 - (2016)',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/e/e5/Summer_Eletrohits_2016.jpg/200px-Summer_Eletrohits_2016.jpg',
-    releaseDate: '2015-12-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562469/summer-eletrohits/12_icx47c.jpg',
+    releaseDate: '2015-12-01T02:00:00.000Z'
   },
   {
     id: 13,
     name: 'Summer Eletrohits 13 (2017)',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5b/Summer_Eletrohits_2017.jpg/200px-Summer_Eletrohits_2017.jpg',
-    releaseDate: '2017-01-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562469/summer-eletrohits/13_dpj3y0.jpg',
+    releaseDate: '2017-01-01T02:00:00.000Z'
   },
   {
     id: 14,
     name: 'Summer Eletrohits 14 (2018)',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/f/f2/Summer_Eletrohits_2018.jpg/200px-Summer_Eletrohits_2018.jpg',
-    releaseDate: '2018-02-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562469/summer-eletrohits/14_elc3cn.jpg',
+    releaseDate: '2018-02-01T02:00:00.000Z'
   },
   {
     id: 15,
     name: 'Summer Eletrohits 15 (2019)',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/7/7d/Summer_Eletrohits_2019.jpg/200px-Summer_Eletrohits_2019.jpg',
-    releaseDate: '2019-02-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562469/summer-eletrohits/15_hqftjx.jpg',
+    releaseDate: '2019-02-01T02:00:00.000Z'
   },
   {
     id: 16,
     name: 'Summer Eletrohits 16 (2020)',
     thumb:
-      'https://upload.wikimedia.org/wikipedia/pt/thumb/a/ab/Summer_Eletrohits_2020.jpg/200px-Summer_Eletrohits_2020.jpg',
-    releaseDate: '2020-01-01T02:00:00.000Z',
+      'https://res.cloudinary.com/viniciushvc/image/upload/v1621562469/summer-eletrohits/16_j0ujrj.jpg',
+    releaseDate: '2020-01-01T02:00:00.000Z'
   }
 ]
